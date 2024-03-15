641408e2fac967528cd84369a31e6a1a0558abeb
刘庆林 <1459307172@qq.com>
2024-03-15 10:20:15 +0800

    feat: docs

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -186,6 +186,7 @@ export function effect<T = any>(
   }
 
   const _effect = new ReactiveEffect(fn)
+  console.log(_effect)
   if (options) {
     extend(_effect, options)
     if (options.scope) recordEffectScope(_effect, options.scope)
